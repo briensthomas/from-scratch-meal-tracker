29e540eb551b85d63da6d6205dc988ce0a3954d6
Brien Thomas <thomasbrien94@gmail.com>
2022-05-02 16:47:21 -0700

    init commit - created ingredient form, grabbed DOM elements relating to form, and have created skeleton event listener for the add ingredient button.

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 // import functions and grab DOM elements
+const ingredientForm = document.getElementById('add-ingredient');
+const ingredientName = document.getElementById('ingredient-name');
+const ingredientQuantity = document.getElementById('ingredient-quantity');
+const ingredientMeasurement = document.getElementById('ingredient-measurement');
+const ingredientSubmit = document.getElementById('ingredient-submit');
 
 // let state
-
+let ingredientList = [];
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+ingredientSubmit.addEventListener('click', (e) => {
+    e.preventDefault();
+    const ingredientData = new FormData(ingredientForm);
+
+    let ingredients = {
+        name: ingredientData.get('ingredient-name'),
+        quantity: ingredientData.get('ingredient-quantity'),
+        measurement: ingredientData.get('ingredient-measurement'),
+    };
+    console.log('logging ingredients', ingredients);
+});
